5b5de751fc5409ceb49a6bb023ae1dc8a303e9cd
Leonardo <34871458+LeozinH1@users.noreply.github.com>
2021-06-01 16:14:11 -0300

    Added log for save comments

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ from src.log import Log
 import requests
 import json
 from src.google_auth import CommentVideo, AuthUser
+from datetime import datetime
+
 log = Log()
+
 from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
 t_duration = 5
 t_icon = "icon.ico"
+
+def append_new_line(file_name, text_to_append):
+    with open(file_name, "a+") as file_object:
+        file_object.seek(0)
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        file_object.write(text_to_append)
+
 
 
 def GetLastVideoId(playlist_id) -> dict:
@@ -44,6 +56,8 @@ def RunBot():
                     CommentVideo(video_id["video_id"], TEXT_COMMENT)
                     last_video = video_id["video_id"]
                     log.info(f"[ {video_id['author_name']} ] Comment sended successfully! Comment: {TEXT_COMMENT}")
+                    time = datetime.now().time().strftime("%H:%M:%S")
+                    append_new_line("log.txt", f"{time} | Comment Published | Channel: {video_id['author_name']} | Video ID: {video_id['video_id']} | Comment: {TEXT_COMMENT}")
                     toaster.show_toast("Comment sended",f"You posted a new comment in the last video of the channel {video_id['author_name']}!", icon_path=t_icon, duration=t_duration)
                 elif last_video == video_id["video_id"]:
                     log.warn(f"[ {video_id['author_name']} ] No new videos found or the comment already been sended!")
--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 # BOT CONFIG
 REFRESH_TIME = 5 # In seconds
 PLAYLIST_ID = "UU2mlpehk_C4Ah6auKV1oLyg" # ID of all videos playlist
-TEXT_COMMENT = "First..." # Text for post in the new video
+TEXT_COMMENT = "First :p" # Text for post in the new video
